0f7ccff02496dc31e8390738b1c7a47dca7ccc4a
Anshi269 <guptanshi4u@gmail.com>
2025-06-12 19:53:33 +0530

    Solved errors

--- backend/server.js
+++ backend/server.js
@@ -8,6 +8,13 @@ app.use(cors());
 app.use(express.json());
 
 
+app.use((req, res, next) => {
+  console.log(`[${new Date().toISOString()}] ${req.method} ${req.originalUrl}`);
+  next();
+});
+
+
+
 
 
 // Use Aunty's Café routes
